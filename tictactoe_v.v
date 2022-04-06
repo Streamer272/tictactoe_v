2a3af3f3fcd01e1097f83bd0d099bdf9b814dede
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-04-06 18:12:12 +0200

    :fire: Deletes useless stuff

--- tictactoe_v.v
+++ tictactoe_v.v
@@ -1,38 +1,5 @@
 module main
 
-/*
-import term
-import term.ui
-import app { App, new_app }
-import field { new_field }
-
-fn main() {
-	mut app := new_app()
-	app.tui = ui.init(
-		user_data: &app
-		cleanup_fn: fn (a voidptr) {
-			mut app := &App(a)
-			app.free()
-		}
-		frame_fn: fn (a voidptr) {
-			mut app := &App(a)
-			app.frame()
-		}
-		event_fn: fn (e &ui.Event, a voidptr) {
-			mut app := &App(a)
-			app.event(e)
-		}
-		fail_fn: fn (error string) {
-			panic(error)
-		}
-		capture_events: true
-		hide_cursor: true
-		frame_rate: 60
-	)
-	app.field = new_field(app.tui)
-	app.tui.run() or { panic(err) }
-}
-*/
 import src
 
 fn main() {
