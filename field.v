6f96ebc89876621260cca347ae0149b648d36a59
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-04-06 08:26:50 +0200

    :zap: Adds handling keyboard events

--- field.v
+++ field.v
@@ -4,9 +4,56 @@ import box { Box }
 
 pub struct Field {
 pub mut:
-	content []Box
+	boxes []Box
 }
 
 pub fn new_field() Field {
-	return Field { content: []Box{len: 9, cap: 9, init: box.new_box(it) } }
+	return Field { boxes: []Box{len: 9, cap: 9, init: box.new_box(it) } }
+}
+
+pub fn (field &Field) display() {
+	for index, box in field.boxes {
+		mut value := ""
+
+		match box.content {
+			.covered, .selected {
+				value = "■"
+			}
+			.uncovered_x {
+				value = "X"
+			}
+			.uncovered_o {
+				value = "O"
+			}
+		}
+
+		match index % 3 {
+			0 {
+				if box.content == .selected {
+					print("[ $value ]")
+				}
+				else if field.boxes[index + 1].content == .selected {
+					print("| $value [")
+				}
+				else {
+					print("| $value |")
+				}
+			}
+			1 {
+				print(" $value ")
+			}
+			2 {
+				if box.content == .selected {
+					println("[ $value ]")
+				}
+				else if field.boxes[index - 1].content == .selected {
+					println("] $value |")
+				}
+				else {
+					println("| $value |")
+				}
+			}
+			else {}
+		}
+	}
 }
--- tictactoe_v.v
+++ tictactoe_v.v
@@ -2,7 +2,50 @@ module main
 
 import field { new_field }
 
+import term
+import term.ui
+
+[heap]
+struct App {
+pub mut:
+	tui &ui.Context = 0
+}
+
+fn (mut a App) event(e &ui.Event) {
+	match e.typ {
+		.key_down {
+			match e.code {
+				.escape, .q {
+					exit(0)
+				}
+				else {}
+			}
+		}
+		else {}
+	}
+}
+
 fn main() {
+	mut app := &App{}
+	app.tui = ui.init(
+		user_data: app,
+		cleanup_fn: fn (a voidptr) {
+			mut app := &App(a)
+			app.free()
+		}
+		event_fn: fn (e &ui.Event, a voidptr) {
+			println("Got event $e on $a")
+			mut app := &App(a)
+			app.event(e)
+		}
+		capture_events: true
+		hide_cursor: true
+		frame_rate: 60
+	)
+	app.tui.run() or { panic(err) }
+
+	/*
 	field := new_field()
-	println("Field: $field")
+	field.display()
+	*/
 }
